807c24b15ff56b21f0bee2c585d119e35b69102e
Vaux Gomes <vauxsandino@gmail.com>
2020-05-24 02:20:41 -0300

    Adicionei uma borda na grade e mudei o calculo de posição para trabalharmos com posicionamento relativos. Também adicionei novas cores ao jogo.

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from pygame.locals import *
 # Variáveis de tela
 WIDTH = 500
 HEIGHT = 500
-ROWS = 20
-BLOCK = WIDTH//ROWS
+BORDER = 50
+ROWS = 10
+BLOCK = (WIDTH - 2*BORDER)//ROWS
 
 # Jogo
 DELAY = 200
@@ -22,6 +23,13 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
+# https://coolors.co/306b34-1c5253-f3ffc6-c3eb78-b6174b
+DARTMOUTH_GREEN = (48, 107, 52)
+DEEP_JUNGLE_GREEN = (28, 82, 83)
+CREAM = (243, 255, 198)
+YELLOW_GREEN = (195, 235, 120)
+CARMINE = (182, 23, 75)
+
 
 class Cube:
     ''' Classe que representa um elemento simples do jogo '''
@@ -32,28 +40,28 @@ class Cube:
 
     def move(self, dir_):
         if dir_ == D_UP:
-            self.position = (self.position[0], self.position[1] - BLOCK)
+            self.position = (self.position[0], self.position[1] - 1)
         elif dir_ == D_DOWN:
-            self.position = (self.position[0], self.position[1] + BLOCK)
+            self.position = (self.position[0], self.position[1] + 1)
         elif dir_ == D_LEFT:
-            self.position = (self.position[0] - BLOCK, self.position[1])
+            self.position = (self.position[0] - 1, self.position[1])
         elif dir_ == D_RIGHT:
-            self.position = (self.position[0] + BLOCK, self.position[1])
+            self.position = (self.position[0] + 1, self.position[1])
 
         # Verificando limite de tela
-        if self.position[0] >= WIDTH:
+        if self.position[0] >= ROWS:
             self.position = (0, self.position[1])
         elif self.position[0] < 0:
-            self.position = (WIDTH - BLOCK, self.position[1])
+            self.position = (ROWS - 1, self.position[1])
 
-        if self.position[1] >= HEIGHT:
+        if self.position[1] >= ROWS:
             self.position = (self.position[0], 0)
         elif self.position[1] < 0:
-            self.position = (self.position[0], HEIGHT - BLOCK)
+            self.position = (self.position[0], ROWS - 1)
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color,
-                         (self.position[0], self.position[1], BLOCK, BLOCK))
+                         (BORDER + self.position[0]*BLOCK, BORDER + self.position[1]*BLOCK, BLOCK, BLOCK))
 
 
 class Snake:
@@ -82,16 +90,21 @@ class Window:
     ''' Classe que exibe os elementos de tela '''
 
     @staticmethod
-    def draw(surface):
+    def draw(surface, points=0):
         # Background
-        surface.fill(BLACK)
+        surface.fill(DEEP_JUNGLE_GREEN)
 
+        u = BORDER
         # Grade
-        for i in range(0, ROWS + 1):
-            u = (i + 1) * BLOCK
 
-            pygame.draw.line(surface, WHITE, (u, 0), (u, HEIGHT))
-            pygame.draw.line(surface, WHITE, (0, u), (WIDTH, u))
+        for i in range(0, ROWS + 1):
+            pygame.draw.line(surface, YELLOW_GREEN,
+                             (u, BORDER), (u, HEIGHT - BORDER))
+
+            pygame.draw.line(surface, YELLOW_GREEN,
+                             (BORDER, u), (WIDTH - BORDER, u))
+
+            u += BLOCK
 
 
 def main():
@@ -104,7 +117,7 @@ def main():
 
     # Elementos
     s = Snake()
-    b = Cube((3*BLOCK, 2*BLOCK), WHITE)
+    b = Cube((3, 2), YELLOW_GREEN)
 
     # Laço principal
     while not exit_:
@@ -132,10 +145,12 @@ def main():
                     dir_ = D_LEFT
                 elif e.key in (K_RIGHT, K_d):
                     dir_ = D_RIGHT
+                elif e.key == K_ESCAPE:
+                    exit_ = True
 
         if s.body[0].position[0] == b.position[0] and s.body[0].position[1] == b.position[1]:
-            b = Cube((rnd.randint(0, ROWS-1)*BLOCK,
-                      rnd.randint(0, ROWS-1)*BLOCK), WHITE)
+            b = Cube((rnd.randint(0, ROWS-1),
+                      rnd.randint(0, ROWS-1)), YELLOW_GREEN)
 
         # Ações
         s.move(dir_)
